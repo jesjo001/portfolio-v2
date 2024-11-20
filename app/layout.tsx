1966defe79577d39f23b9f6a1bb1852c9d3954c4
jesjo <automationlounge@gmail.com>
2024-11-20 12:17:36 +0100

    page description updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Josh's Portfolio ",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  description: "Automation Lounge | Technology Blog",
 };
 
 export default function RootLayout({
